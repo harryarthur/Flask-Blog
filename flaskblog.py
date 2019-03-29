bf864b8552786f54d2b3811259befdd4acf137e5
harryarthur <harry.arthur1993@gmail.com>
2019-03-29 12:47:29 +1200

    started with login and register templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+#When using the sign up and log in forms we need a secret key to protect against modifying cookies
+#and forgery attacks etc. app.config is used to set a config value on our application
+#You want your secret key to be a good set of random characters.(you can use pythons secret module 
+# to generate one).You'll likely want to make this an environment vairable at some point.
+app.config['SECRET_KEY'] = '12411fdc9f8812de6e1797a7934cd3e2'
 
 posts = [
     {
@@ -24,7 +31,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
-    
+
+
+@app.route("/register")
+def register():
+#we need to create an instance of the form to send to our application 
+    form = RegistrationForm() 
+#and then we can pass this form to a template
+    return render_template('register.html', title = 'Register', form=form)   
+
+@app.route("/login")
+def login():
+  form = LoginForm()
+  return render_template('login.html',title='Login', form=form)
 
 #this conditional is only true if we run this script directly using python
 #if running through flask, remember to set environment variables:
--- forms.py
+++ forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, Submit
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+#The most popular extension for working with forms in flask is called wtforms (wtf)
+#Using this flask extension we will write python classes that represent
+#the forms we want to create. These classes will then be automatically be converted
+#into HTML forms within our template. 
+# 
+#Each form you want will need its own class and all form fields will be imported classes as well:
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
@@ -12,11 +18,13 @@ class RegistrationForm(FlaskForm):
 
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('Password')])
 
-    submit = Submit('Sign Up')
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',validators=[DataRequired(),Email()])
 
     password = PasswordField('Password',validators=[DataRequired()])
 
-    submit = Submit('Sign Up')
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
