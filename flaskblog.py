a0aa05b86836ab84a7f11cccdf615c9455219f33
harryarthur <harry.arthur1993@gmail.com>
2019-03-30 17:18:47 +1200

    added sqlite db with SQLAlchemy with 2 classes

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 #When using the sign up and log in forms we need a secret key to protect against modifying cookies
 #and forgery attacks etc. app.config is used to set a config value on our application
@@ -6,6 +8,46 @@ from forms import RegistrationForm, LoginForm
 # to generate one).You'll likely want to make this an environment vairable at some point.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#SQLAlchemy is an ORM (Object relational Mapper) - it allows u to acces our database in 
+#an easy to use object oriented way. SQl Alchemyis cool because you can use idfferent databases 
+#without changing your python code underneath. So for example, if you want to use an SQLite db for
+#testing and a postgres db for production, then all you need to do is pass a different db url for
+#sqlalchemy to connect and all the code to CRUD the db will remain the same. sqlite is a file
+#on the file system.
+
+#create a DB instance
+db = SQLAlchemy(app)
+#sqlalchemy lets us represent our database as classes called models like this:
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default = 'default.jpeg')
+    password = db.Column(db.String(60), nullable=False)
+#backref attribute is similiar to adding anothing column to this middle, but what it
+#allows us to do is when we have a post we can simply use this author attribute to get the
+#user who created the post. The lazy argument just defines how SQLalchemy lods the data
+#from the database setting it to True means it will load it as necessary in one go
+#capital P in Post because we're referencing the Post class itself
+    posts = db.relationship('Post',backref='author',lazy=True)
+#double underscore method(dunder methods or magic methods), this specifically is how
+#our object is printed whenever we print it out. This function is part of the User class
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}', '{self.image_file}')" 
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+#user.id, user is lowercase U because it is refering the table name and the column name
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable = False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')" 
 
 posts = [
     {
