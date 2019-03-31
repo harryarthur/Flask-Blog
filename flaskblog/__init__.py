cfc1c6b57988e62c9f89eee7b0c51d8a3399f68d
harryarthur <harry.arthur1993@gmail.com>
2019-03-31 23:14:39 +1200

    added user auth and account

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 #When using the sign up and log in forms we need a secret key to protect against modifying cookies
 #and forgery attacks etc. app.config is used to set a config value on our application
 #You want your secret key to be a good set of random characters.(you can use pythons secret module 
@@ -16,5 +18,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 #create a DB instance
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
