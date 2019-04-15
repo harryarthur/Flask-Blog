e24e8b2f8940eab941c9ac9a26d41e7e29fd16ed
harryarthur <harry.arthur1993@gmail.com>
2019-04-15 19:05:14 +1200

    restructured using blue prints and moved configuration into its own file and created a configuration class. moved the creation of application into its own function

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,16 +1,7 @@
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_mail import Mail
 #When using the sign up and log in forms we need a secret key to protect against modifying cookies
 #and forgery attacks etc. app.config is used to set a config value on our application
 #You want your secret key to be a good set of random characters.(you can use pythons secret module 
 # to generate one).You'll likely want to make this an environment vairable at some point.
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #SQLAlchemy is an ORM (Object relational Mapper) - it allows u to acces our database in 
 #an easy to use object oriented way. SQl Alchemyis cool because you can use idfferent databases 
 #without changing your python code underneath. So for example, if you want to use an SQLite db for
@@ -18,17 +9,37 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #sqlalchemy to connect and all the code to CRUD the db will remain the same. sqlite is a file
 #on the file system.
 
-#create a DB instance
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USER_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-mail = Mail(app)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
-from flaskblog import routes
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
+
